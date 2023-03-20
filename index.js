130441c73c71e42dadbb7278177d2d1fb5b272a6
jonathankerth <jonathanpkerth@gmail.com>
2023-03-20 14:22:06 -0700

    added body-parser middlewear

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const bodyParser = require('body-parser');
+
 const express = require('express'),
 morgan = require('morgan');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
