f24280739e51a2c781ffb7a74a73388a4a85e98f
jonathankerth <jonathanpkerth@gmail.com>
2023-03-21 08:45:37 -0700

    deleted array of movies and organized

--- index.js
+++ index.js
@@ -32,59 +32,7 @@ let myLogger = (req, res, next) => {
   app.use(myLogger);
   app.use(requestTime);
 
-  const topMovies = [
-    {
-      title: "The Godfather",
-      director: "Francis Ford Coppola",
-      year: 1972
-    },
-    {
-      title: "The Shawshank Redemption",
-      director: "Frank Darabont",
-      year: 1994
-    },
-    {
-      title: "The Dark Knight",
-      director: "Christopher Nolan",
-      year: 2008
-    },
-    {
-        title: "Finding Nemo",
-        director: "Andrew Stanton",
-        year: 2003
-      },
-      {
-        title: "Finding Dory",
-        director: "Andrew Stanton",
-        year: 2016
-      },
-      {
-        title: "National Treasure",
-        director: "Jon Turteltaub",
-        year: 2004
-      },
-      {
-        title: "National Treasure 2",
-        director: "Jon Turteltaub",
-        year: 2007
-      },
-      {
-        title: "Lord of War",
-        director: "Andrew Niccol",
-        year: 2005
-      },
-      {
-        title: "Ghost Rider",
-        director: "Mark Steven Johnson",
-        year: 2007
-      },
-      {
-        title: "Pig",
-        director: "Michael Sarnoski",
-        year: 2021
-      },
-  ];
-  
+
 // Return a list of ALL movies to the user
 app.get('/movies', (req, res) => {
   res.send(topMovies);
@@ -167,16 +115,6 @@ app.delete('/users/:email', (req, res) => {
   const email = req.params.email;
   res.send(`Allow user with email ${email} to deregister`);
 });
-
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
-
-  app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
-
   // Get all users
 app.get('/users', (req, res) => {
   Users.find()
@@ -260,4 +198,12 @@ app.delete('/users/:Username', (req, res) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
     });
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
 });
