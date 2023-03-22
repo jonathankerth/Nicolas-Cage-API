ae5240b78ee9aa4d281d2c35df5f8c34f589dad8
jonathankerth <jonathanpkerth@gmail.com>
2023-03-22 11:09:09 -0700

    changed back to 8080

--- index.js
+++ index.js
@@ -204,6 +204,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-app.listen(3000, () => {
-  console.log('Your app is listening on port 3000.');
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
 });
