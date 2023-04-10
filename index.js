4ca4aa192f086c53aa27ca3f47175a2e3ae50ff2
jonathankerth <jonathanpkerth@gmail.com>
2023-04-10 10:59:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ const bodyParser = require('body-parser');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('MongoDB connected successfully');
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static('public'));
 
