0d3d03c51142fb00979ebfc9e7372ef2c60e070f
jonathankerth <jonathanpkerth@gmail.com>
2023-04-10 12:19:59 -0700

    add passport auth to GET users

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post('/users', [
 });
 
 // Get all users
-app.get('/users', (req, res) => {
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
