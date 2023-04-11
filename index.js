e510b3d4f8765d14b5859b25a3957c143df1e120
jonathankerth <jonathanpkerth@gmail.com>
2023-04-11 16:15:38 -0700

    added cors options

--- index.js
+++ index.js
@@ -15,13 +15,28 @@ mongoose.connect(process.env.CONNECTION_URI, {
   useUnifiedTopology: true,
 })
 
-const corsOptions = {
-  origin: 'http://localhost:1234', // Replace with your client's origin
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+let allowedOrigins = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'http://testsite.com',
+  'https://myflixdb.herokuapp.com',
+]
 
-app.use(cors(corsOptions))
-
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          'The CORS policy for this application doesn’t allow access from origin ' +
+          origin
+        return callback(new Error(message), false)
+      }
+      return callback(null, true)
+    },
+  })
+)
 app.use(express.static('public'))
 
 app.use(morgan('common'))
