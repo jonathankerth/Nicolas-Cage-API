aa90bd71edde0f056d8a87184dca1d94eb428abe
jonathankerth <jonathanpkerth@gmail.com>
2023-04-26 13:50:03 -0700

    COR update, remove app.use block

--- index.js
+++ index.js
@@ -23,18 +23,9 @@ let allowedOrigins = [
   'https://niccage.herokuapp.com',
   'https://niccagecllient.netlify.app',
   'https://main--niccagecllient.netlify.app/',
+  'https://niccagecllient.netlify.app/',
   '*',
 ]
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  next()
-})
 
 app.use(
   cors({
